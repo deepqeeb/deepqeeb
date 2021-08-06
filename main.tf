c81bd17115fbbd148a4b6b95f406db9526ca48e3
deepqeeb <87971462+deepqeeb@users.noreply.github.com>
2021-08-06 15:04:26 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
-resource null_resource "example" {}
- output "test_1" {
-    value = "test_1"
- }
- variable "test_variable" {}
- 
+# resource null_resource "example" {}
+#  output "test_1" {
+#     value = "test_1"
+#  }
+#  variable "test_variable" {}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0"
+  # insert the 19 required variables here
+}
+
